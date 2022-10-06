ffa16729c9b2c2b10995e52f772a3b84dc0a68c6
Opal_v <Mvvento@gmail.com>
2022-10-05 21:47:30 -0400

    Add files via upload

---
+++ Balanced_parenth.py
@@ -0,0 +1,1 @@
+def Balanced_parenth
---
+++ find_match.py
@@ -0,0 +1,31 @@
+# given a list of words, and a word that has some letters missing, but '.' in place of the missing letters (e.g "h.llo),
+#  return a word from the list that matches the imcomplete word (e.g. "hello")
+
+
+
+
+
+# function because we are asked to find  a word out of a list
+# Not coplicated enough for a class. No inheritance logic etc.locals
+
+
+# return type -> string: "return a word from the list"
+# name of the function -> call this whatever "find_word_match"
+# params/input -> list word
+# body, main logic: conditions, loops
+
+
+# create a list with a missing letter
+
+# find the word with a missin letter "."
+# loop through 
+
+
+def Find_word_match (words: List[str], match: str) -> str:
+
+
+# c++ std:: string FindWordMatch(
+
+
+
+
